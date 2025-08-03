a854c191b0dc5da26057668adbb1935dedca86de
ingIsaiasHernandez <ing.isaias.hdez@gmail.com>
2025-08-03 04:21:12 -0600

    [add] - se instalaron las dependencias necesarias para el proyecto.
    -se intaló express, mongoose, nodemon, dotenv.
    -se creó el archivo .env para manejar las variables de entorno.
    -se creó el archivo server.js como punto de entrada del servidor.
    -se creó el archivo .gitignore para ignorar node_modules y otros archivos innecesarios.
    -se conecto la base de datos MongoDB usando mongoose.

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+console.log(process.env.MONGO_URI);
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server is running at http://localhost:5000");
+});
