8a8a457be8ac60e358c52d4fb5264eb287f0887c
ingIsaiasHernandez <ing.isaias.hdez@gmail.com>
2025-08-04 21:29:02 -0600

    [add] - se creo el navbar, el body y formulario para crear el producto

---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,39 @@
+import { Box, Button, Container, Heading, Input, useColorModeValue, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+
+const CreatePage = () => {
+    const [newProduct, setNewProduct] = useState({
+        name: "",
+        price: "",
+        image: "",
+    });
+
+    const handleAddProduct = () => {
+        // Logic to handle adding a new product
+        console.log("Product added:", newProduct);
+
+    }
+    return (
+        <Container maxW={"container.sm"}>
+            <VStack spacing={8}>
+                <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+                    Create New Product
+                </Heading>
+                <Box w={"full"} bg={useColorModeValue("white", "gray.800")} p={6} rounded={"lg"} shadow={"md"}>
+
+                    <VStack spacing={4}>
+                        <Input placeholder="Product name" name="name" value={newProduct.name} onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })} />
+                        <Input placeholder="Price" name="price" type="number" value={newProduct.price} onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })} />
+                        <Input placeholder="Image URL" name="image" value={newProduct.image} onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })} />
+                        <Button colorScheme="blue" onClick={handleAddProduct}>
+                            Add Product
+                        </Button>
+
+                    </VStack>
+                </Box>
+            </VStack>
+        </Container>
+    )
+}
+
+export default CreatePage
